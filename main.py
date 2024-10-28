3b6997269f62a59c90906384857a8ae25bb780bb
Leo Gimenez <leogimenez.dev@gmail.com>
2024-10-28 12:03:31 -0300

    se trabaja funcion que calcula

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from tkcalendar import DateEntry
 import datetime
 
 def calcular_vacaciones():
-    
+    # Obtener la fecha de ingreso desde el calendario
+    fecha_ingreso = calendario.get_date()
